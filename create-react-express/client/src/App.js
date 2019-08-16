f2eeae7158e897b3b4b4fd78d74e836e79658137
Amir Bar <havainc@gmail.com>
2019-08-16 15:27:48 -0700

    cards spacing

--- create-react-express/client/src/App.js
+++ create-react-express/client/src/App.js
@@ -3,14 +3,28 @@ import logo from "./logo.svg";
 import "./App.css";
 import Nav from "./components/navtab";
 import Card from "./components/card";
+import Grid from "@material-ui/core/Grid";
+// import { spacing } from '@material-ui/system';
 
+// const theme = {
+//   spacing: [10,20,30]
+// }
 
 function App() {
   return (
     <div>
       <Nav />
-      <Card />
+      <Grid container direction="row" justify="center" alignItems="center" >
+        <Card />
+        <Card />
+        <Card />
+        <Card />
 
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </Grid>
     </div>
   );
 }
