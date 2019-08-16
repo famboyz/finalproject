6036f83cedd591f7c139707ac75f87dd26bbdd77
Amir Bar <havainc@gmail.com>
2019-08-16 14:19:43 -0700

    card

--- create-react-express/client/src/App.js
+++ create-react-express/client/src/App.js
@@ -2,11 +2,15 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Nav from "./components/navtab";
+import Card from "./components/card";
+
 
 function App() {
   return (
     <div>
       <Nav />
+      <Card />
+
     </div>
   );
 }
