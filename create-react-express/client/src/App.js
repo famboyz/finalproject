2a778809abc8658d1a4423c9518c9d6237ca33f3
Amir Bar <havainc@gmail.com>
2019-08-16 13:42:18 -0700

    navbar

---
+++ create-react-express/client/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Nav from "./components/navtab";
+
+function App() {
+  return (
+    <div>
+      <Nav />
+    </div>
+  );
+}
+
+export default App;
